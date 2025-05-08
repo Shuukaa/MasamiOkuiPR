fcbd7a48d642c8952c08a5217a65abd9d5daa730
Shuukaa <69614231+Shuukaa@users.noreply.github.com>
2025-05-08 01:29:10 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+const config = {
+    localStoragePrefix: "party-rank-sorter",
+    title: "Masami Okui Party Rank Sorter",
+    description: "Masami Okui Party rank sorter for your list of songs."
+};
